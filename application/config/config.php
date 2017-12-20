c85d0069ab4dba6c902356eb60c6d2ed1a7fd421
Lisanne <lisannehofstede18@gmail.com>
2017-12-20 16:34:35 +0100

    Config updated+Libraries autoload function

---
+++ application/config/config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lisanne
+ * Date: 20-12-2017
+ */
+
+// Applicatie Root
+    define('APPROOT',dirname(dirname(__FILE__)));
+
+// URL Root
+    define('URLROOT', 'http://localhost/Frameworkmvc/');
+
+// Site Naam
+    define('SITENAME', 'Frameworkmvc');
